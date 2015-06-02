090f8f170c21aac1247f1255148e25a10a9a7b29
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2015-06-02 13:43:14 +0200

    introduction of child view (and controller view)

--- client.js
+++ client.js
@@ -1,25 +1,18 @@
-/*global document, window */
-
 import Fluxible from 'fluxible';
 import React from 'react';
-import {connectToStores, provideContext} from 'fluxible/addons';
-import FluxibleComponent from 'fluxible/addons/FluxibleComponent';
 
 import HelloMessage from './components/HelloMessage';
 import MessageStore from './stores/MessageStore';
 import action from './actions/messageAction';
 
 const app = new Fluxible({
-    component: provideContext(connectToStores(HelloMessage, [MessageStore], (stores) => {
-        return {
-            greeting: stores.MessageStore.message
-        };
-    })),
+    component: HelloMessage,
     stores: [MessageStore]
 });
 
-const mountNode = document.getElementById('example');
 const context = app.createContext();
-context.executeAction(action, 'Hello', err => {
+const mountNode = document.getElementById('example');
+const defaultGreeting = 'Hello';
+context.executeAction(action, defaultGreeting, err => {
     React.render(context.createElement(), mountNode);
 });
--- components/HelloMessage.js
+++ components/HelloMessage.js
@@ -1,36 +1,50 @@
 import React from 'react';
+import {connectToStores, provideContext} from 'fluxible/addons';
 import MessageStore from '../stores/MessageStore';
 import action from '../actions/messageAction';
+import MessageDisplay from './MessageDisplay';
 
-export default class HelloMessage extends React.Component {
+class HelloMessage extends React.Component {
     static get contextTypes() {
         return {
             getStore: React.PropTypes.func.isRequired,
             executeAction: React.PropTypes.func.isRequired
         };
     }
+
     constructor(props) {
         super(props);
     }
+
     reset() {
         this._sendGreeting('');
         this.refs.in.getDOMNode().focus();
     }
+
     updateModel(event) {
         this._sendGreeting(event.target.value);
     }
+
     _sendGreeting(greeting) {
         this.context.executeAction(action, greeting);
     }
+
     render() {
         return (
-        <div>
-            <input ref="in"
-                   onChange={this.updateModel.bind(this)}
-                   value={this.props.greeting} />
-            <p>{this.props.greeting}, World</p>
-            <button
-                   onClick={this.reset.bind(this)}>Clear</button>
-        </div>);
+            <div>
+                <input ref="in"
+                       onChange={this.updateModel.bind(this)}
+                       value={this.props.greeting}/>
+                <MessageDisplay message={this.props.greeting + ', World'}/>
+                <button
+                    onClick={this.reset.bind(this)}>Clear
+                </button>
+            </div>);
     }
 }
+
+export default provideContext(connectToStores(HelloMessage, [MessageStore], (stores) => {
+    return {
+        greeting: stores.MessageStore.message
+    };
+}));
